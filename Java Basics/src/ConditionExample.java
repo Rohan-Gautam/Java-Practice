c7915747e3fb46865a818c4e8c51700d2dfc59d1
Name <136186521+Rohan-Gautam@users.noreply.github.com>
2025-05-31 23:04:57 +0530

    data types, conditions, switch

---
+++ Java Basics/src/ConditionExample.java
@@ -0,0 +1,13 @@
+public class ConditionExample {
+    public static void main(String[] args) {
+        int score = 82;
+
+        if (score >= 90 ) {
+            System.out.println("Grade A");
+        } else if (score >= 75) {
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Grade C");
+        }
+    }
+}
---
+++ Java Basics/src/DataTypesExample.java
@@ -0,0 +1,15 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int age = 21;
+        double height = 5.10;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "Rohan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+    }
+}
